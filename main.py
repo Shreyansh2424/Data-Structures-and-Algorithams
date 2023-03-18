631be62cf10659113f7c306dec7dfab313c25743
shreyansh kapdi <117802342+Shreyansh2424@users.noreply.github.com>
2023-03-18 14:06:50 +0000

    Lesson -1 (Introduction to Python)

---
+++ main.py
@@ -0,0 +1,60 @@
+#First Program in Python:
+
+#print("Hello World")
+#print("I want to be a successful person ")
+
+# ------------------------------------------------------
+
+#Implementing Variables in Python:
+
+#a = 10
+#b = 20
+#print(a + b)
+#print("(a+b)")
+
+#a = "I want to become a billionaire"
+#print(a)
+
+# -------------------------------------------------------
+
+#Data Types in Python:
+
+#a = 20
+#print(type(a))
+#b = "coding"
+#print(type(b))
+#c = 5.4
+#print(type(c))
+
+# -------------------------------------------------------
+
+#Arithmetic Operators in Python:
+
+# 1. (** = to find the power of any number. )
+#a = 20
+#b = 2
+#c = a**b
+#print(c)
+
+#2.(// = Flow Division always gives you an Integer answer)
+#a = 17
+#b = 10
+#print(a / b)
+#print(a // b)
+
+# -------------------------------------------------------
+
+#Taking Inputs in Python:
+
+#1. (Inputs in Python are by default stored as a String)
+#a = input()
+#print(a)
+#print(type(a))
+
+#2. (How to convert String to Integer)
+a = int(input())
+print(a)
+print(type(a))
+
+
+
