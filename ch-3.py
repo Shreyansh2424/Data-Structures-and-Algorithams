12fa35687cb817d3062b6b45b7a839da3634a063
shreyansh kapdi <117802342+Shreyansh2424@users.noreply.github.com>
2023-03-23 07:39:43 +0000

    Chapter -1 Introduction to Python

---
+++ ch-3.py
@@ -0,0 +1,12 @@
+#CH-3(Introduction to Algorithm in python)
+
+# Lesson-1(Algorithms and its characteristics):
+
+#Q-1. What is an Algorithm?
+#Ans.Step by Step Procedure.
+
+#Q-2. Characteristics of Algorithm
+#Ans.1- An Algorithm should be Independent of every Coding language.
+#     2- It should have definite answer
+
+#-----------------------------------------------------------------------------------------------
